883d10082b5063a2801f7adf3f4ec7267a7e5418
wiseknown <wiseknown@users.noreply.github.com>
2020-04-10 02:41:03 -0500

    Creat of some initial function and test GIT funcionality

---
+++ Code/Initial.m
@@ -0,0 +1,8 @@
+function [outputArg1,outputArg2] = Initial(inputArg1,inputArg2)
+%INITIAL Set up the initial variables of the model
+% Input arguments values
+%
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
