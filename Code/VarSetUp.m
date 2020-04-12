c52288c2528c90d48cc7edc08fd62d1381f4b6be
wiseknown <wiseknown@users.noreply.github.com>
2020-04-12 08:52:13 -0500

    VarSetUp function is written initially. Main information inside the function itself.

---
+++ Code/VarSetUp.m
@@ -0,0 +1,30 @@
+function [f,feq,u,v,rho,cx,cy] = VarSetUp(n,m,dimen,dis_velo)
+%INITIAL Set up the initial variables of the model
+% ---INPUT PARAMETERS---
+% n       --> number of nodes on x coord
+% m       --> number of nodes on y coord
+% dimen   --> dimension, 2 or 3,D (numerical value)
+% dis_velo--> number of discrete velocities (numerical value) 
+%
+% ---OUTPUT PARAMETERS---
+% f       --> distribution functions per each node (x,y,..) dimension
+% feq     --> equilibrium distribution functions per each node
+% v       --> y axis velocity field (not macroscopic)
+% u       --> x axis velocity field (not macroscopic)
+
+if dimen == 2
+    f   = zeros(n,m,dis_velo);
+    feq = zeros(dis_velo,n*m);
+    v   = zeros(dis_velo,n*m);
+    u   = zeros(dis_velo,n*m);
+    rho = ones(n,m);
+    switch dis_velo
+        case 9
+            cx = [0, 1, 0, -1, 0, 1, -1, -1, 1];
+            cy = [0, 0, 1, 0, -1, 1, 1, -1, -1];
+    end
+else
+    %para casos de 3D 
+end
+end
+
