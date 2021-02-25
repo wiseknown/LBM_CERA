5ac2fd54a3876a3acaed2dfde688c92623e565fc
wiseknown <wiseknown@users.noreply.github.com>
2021-02-24 21:57:16 -0500

    Start New Project 2021

--- Code/Streaming.m
+++ Code/Streaming.m
@@ -2,7 +2,7 @@ function [f] = Streaming(f,dis_velo,cx,cy,n,m)
 %STREAMING Streaming function depends on number of discrete velocities in
 %this case it just able to do it in 2D
 % f output is rearraging to n x m x dis_velo in 3D 
-% After setting BC you should rearraging again 
+% After setting BC you should use function ReArraging to reorder the nodes 
 %   cx and cy are setting in the VarSetUp function
     f = reshape(f,[dis_velo,n,m]);
     f = permute(f,[2,3,1]);
--- Test.m
+++ Test.m
@@ -9,13 +9,21 @@
 % s_0 to s_6 -> Liu et al. (2014) doi: j.ijheatmasstransfer.2014.02.047
 % s_7 and s_8 belongs to [0.89,1.17] doi: 10.1016/j.jcp.2014.10.038
 % This interval is optimal but not exclusive
+
 S = [1.0; 1.1; 1.1; 1.0; 1.2; 1.0; 1.2; 1.0; 1.0];
 S = SetCollider(S,5,5);
-%Main Loop 
-MM = Momentum(2,9,f,M);
-% rho index 1
-% u index 4
-% v index 6
-eq = Equilibrium(eq,MM(1,:),MM(4,:),MM(6,:),'MRT-GS');
-f = CollesionMRT2D(eq,S,MM,Minv);
-f = Streaming(f,9,cx,cy,5,5);
+
+iter = 1;
+
+%Main Loop
+while iter > 2
+    MM = Momentum(2,9,f,M);
+    % rho index 1
+    % u index 4
+    % v index 6
+    eq = Equilibrium(eq,MM(1,:),MM(4,:),MM(6,:),'MRT-GS');
+    f = CollesionMRT2D(eq,S,MM,Minv);
+    f = Streaming(f,9,cx,cy,5,5);
+    f = ReArraging(f,9,5,5);
+    iter = iter + 1;
+end
