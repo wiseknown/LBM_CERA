68331c6c8204f149186ed06be65d15147c46c0c8
wiseknown <wiseknown@users.noreply.github.com>
2021-03-21 00:28:11 -0500

    Add new approach to set initial variables
    The use of f_nodes or fluid nodes is started in this commit. All the
    positions are gonna be saved in a logic matrix in another function.
    Tests are not done yet.

--- Code/VarSetUp.m
+++ Code/VarSetUp.m
@@ -1,32 +1,43 @@
-function [f,eq,u,v,rho,cx,cy] = VarSetUp(n,m,dimen,dis_velo)
+function [f,eq,u,v,rho,cx,cy,cz] = VarSetUp(f_nodes,dis_velo,varargin)
 %INITIAL Set up the initial variables of the model
 % ---INPUT PARAMETERS---
-% n       --> number of nodes on x coord
-% m       --> number of nodes on y coord
-% dimen   --> dimension, 2 or 3,D (numerical value)
+% f_nodes --> number of fluid nodes in the media
 % dis_velo--> number of discrete velocities (numerical value) 
-%
+% dimen   --> dimension, 2 or 3,D (numerical value) - Optional Argument
+% 
 % ---OUTPUT PARAMETERS---
-% f       --> distribution functions per each node (x,y,..) dimension
-% eq      --> equilibrium distribution functions or momentum equilibrium functions
-%             per each node
+% f       --> distribution functions per each fluid node (x,y,..) dimension
+% eq      --> equilibrium distribution functions or momentum equilibrium 
+%             functions per each node 
 % v       --> y axis velocity field (not macroscopic)
 % u       --> x axis velocity field (not macroscopic)
 % rho     --> density field (not macroscopic)
+%
+% ---COMMENTS---
+% When use 3D is necessary to specify dimension 3 in optional arguments
+%
 
-if dimen == 2
-    f   = zeros(dis_velo,n*m);
-    eq = zeros(dis_velo,n*m);
-    v   = zeros(1,n*m);
-    u   = zeros(1,n*m);
-    rho = ones(1,n*m);
+
+f   = zeros(dis_velo,f_nodes);
+eq  = zeros(dis_velo,f_nodes);
+v   = zeros(1,f_nodes);
+u   = zeros(1,f_nodes);
+rho = ones(1,f_nodes);
+
+if size(varargin) == 0x0
     switch dis_velo
         case 9
             cx = [0, 1, 0, -1, 0, 1, -1, -1, 1];
             cy = [0, 0, 1, 0, -1, 1, 1, -1, -1];
     end
 else
-    %para casos de 3D 
+    switch dis_velo
+        case 15
+            cx = [0, 1, 0, -1, 0, 1, -1, -1, 1];
+            cy = [0, 1, 0, -1, 0, 1, -1, -1, 1];
+            cz = [0, 0, 1, 0, -1, 1, 1, -1, -1];
+    end
 end
+
 end
 
