6ea1573f8295dc4de87e4598fa3232aec8570461
wiseknown <wiseknown@users.noreply.github.com>
2021-03-22 00:01:04 -0500

    Add 3D functionality
    The function start to create transformation matrix based on Grand-Schmid
    Details are in the documentation at the begining of the function.
    Documentation is explained and added in this commit.

--- Code/MRT_Matrix.m
+++ Code/MRT_Matrix.m
@@ -1,6 +1,19 @@
 function [M,Minv] = MRT_Matrix(dimen,dis_velo,scheme)
 %MRT_MATRIX Set Matrix distribution to momentum space transformation
-%   Detailed explanation goes here
+% ---INPUT PARAMETERS---
+% dimen   --> dimension, 2 or 3,D (numerical value)
+% dis_velo--> number of discrete velocities (numerical value) 
+% scheme  --> 1 opt: MRT-GS MRT Grand-Schmidt 
+% 
+% ---OUTPUT PARAMETERS---
+% M       --> transformation matrix
+% Minv    --> Inverse of transformation matrix
+%
+% ---COMMENTS---
+% When use 3D is necessary to specify dimension 3 in optional arguments
+%
+% The matrices are based on section 10.4.2 for 2D MRT and Appendix A.6 
+% (page 669-672) for 3D MRT.
 if dimen == 2
     switch scheme
         case 'MRT-GS'
@@ -17,6 +30,50 @@ if dimen == 2
                 Minv = inv(M);
             end
     end
+elseif dimen == 3
+    switch scheme
+        case 'MRT-GS'
+            if dis_velo == 15
+                M = [ 1  1  1  1  1  1  1  1  1  1  1  1  1  1  1;
+                     -2 -1 -1 -1 -1 -1 -1  1  1  1  1  1  1  1  1;
+                     16 -4 -4 -4 -4 -4 -4  1  1  1  1  1  1  1  1;
+                      0  1 -1  0  0  0  0  1 -1  1 -1  1 -1 -1  1;
+                      0 -4  4  0  0  0  0  1 -1  1 -1  1 -1 -1  1;
+                      0  0  0  1 -1  0  0  1 -1  1 -1 -1  1  1 -1;
+                      0  0  0 -4  4  0  0  1 -1  1 -1 -1  1  1 -1;
+                      0  0  0  0  0  1 -1  1 -1 -1  1  1 -1  1 -1;
+                      0  0  0  0  0 -4  4  1 -1 -1  1  1 -1  1 -1;
+                      0  2  2 -1 -1 -1 -1  0  0  0  0  0  0  0  0;
+                      0  0  0  1  1 -1 -1  0  0  0  0  0  0  0  0;
+                      0  0  0  0  0  0  0  1  1  1  1 -1 -1 -1 -1;
+                      0  0  0  0  0  0  0  1  1 -1 -1 -1 -1  1  1;
+                      0  0  0  0  0  0  0  1  1 -1 -1  1  1 -1 -1;
+                      0  0  0  0  0  0  0  1 -1 -1  1 -1  1 -1  1];
+                Minv = inv(M);
+            elseif dis_velo == 19
+                M = [  1   1   1   1   1   1   1  1  1  1  1  1  1  1  1  1  1  1  1;
+                     -30 -11 -11 -11 -11 -11 -11  8  8  8  8  8  8  8  8  8  8  8  8;
+                      12  -4  -4  -4  -4  -4  -4  1  1  1  1  1  1  1  1  1  1  1  1;
+                       0   1  -1   0   0   0   0  1 -1  1 -1  0  0  1 -1  1 -1  0  0;
+                       0  -4   4   0   0   0   0  1 -1  1 -1  0  0  1 -1  1 -1  0  0;
+                       0   0   0   1  -1   0   0  1 -1  0  0  1 -1 -1  1  0  0  1 -1;
+                       0   0   0  -4   4   0   0  1 -1  0  0  1 -1 -1  1  0  0  1 -1;
+                       0   0   0   0   0   1  -1  0  0  1 -1  1 -1  0  0 -1  1 -1  1;
+                       0   0   0   0   0  -4   4  0  0  1 -1  1 -1  0  0 -1  1 -1  1;
+                       0   2   2  -1  -1  -1  -1  1  1  1  1 -2 -2  1  1  1  1 -2 -2;
+                       0  -4  -4   2   2   2   2  1  1  1  1 -2 -2  1  1  1  1 -2 -2;
+                       0   0   0   1   1  -1  -1  1  1 -1 -1  0  0  1  1 -1 -1  0  0;
+                       0   0   0  -2  -2   2   2  1  1 -1 -1  0  0  1  1 -1 -1  0  0;
+                       0   0   0   0   0   0   0  1  1  0  0  0  0 -1 -1  0  0  0  0;
+                       0   0   0   0   0   0   0  0  0  0  0  1  1  0  0  0  0 -1 -1;
+                       0   0   0   0   0   0   0  0  0  1  1  0  0  0  0 -1 -1  0  0;
+                       0   0   0   0   0   0   0  1 -1 -1  1  0  0  1 -1 -1  1  0  0;
+                       0   0   0   0   0   0   0 -1  1  0  0  1 -1  1 -1  0  0  1 -1;
+                       0   0   0   0   0   0   0  0  0  1 -1 -1  1  0  0 -1  1  1 -1];
+                Minv = inv(M);
+            end
+    end
 end
 end
+
 
