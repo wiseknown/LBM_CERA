0989e4e9527acfb995368a0e64e17c3f6701db6f
wiseknown <wiseknown@users.noreply.github.com>
2021-03-27 12:59:29 -0500

    Minor changes in documentation

--- Code/Equilibrium.m
+++ Code/Equilibrium.m
@@ -1,10 +1,11 @@
-function [eq] = Equilibrium(rho,u,v,dis_velo,dimen,scheme)
+function [eq] = Equilibrium(rho,vel,dis_velo,dimen,scheme)
 %Equilibrium --> Provides the equilibrium distribution function for BGK and
 %MRT Grand Schmidt schemes in 2 and 3 dimensions
 %
 %---INPUT PARAMETER---
-% rho     -->
-% 
+% rho     --> density field 
+% vel     --> Cell type compose from velocity in x (u), velocity in y (v)
+%             and velocity in z (w)
 % dis_velo--> number of discrete velocities (numerical value) 
 % dimen   --> dimension, 2 or 3,D (numerical value) 
 % scheme  --> 1 opt: MRT-GS MRT Grand-Schmidt
@@ -18,6 +19,8 @@ function [eq] = Equilibrium(rho,u,v,dis_velo,dimen,scheme)
 %     & Viggen, E. M. (2017). The lattice Boltzmann method. Springer 
 %     International Publishing, 10, 978-3.
 if dimen == 2
+    u = vel{1};
+    v = vel{2};
     switch scheme
         case 'BGK'
             if dis_velo == 9
@@ -53,6 +56,7 @@ if dimen == 2
     end
 else
     %3d cases
+    w = vel{3};
     switch scheme
         case 'MRT-GS'
             if dis_velo == 15
