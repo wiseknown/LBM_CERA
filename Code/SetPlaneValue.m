96509f2b8a3a60589058e927149b82804bdc8095
wiseknown <wiseknown@users.noreply.github.com>
2021-04-05 20:24:24 -0500

    SetPlaneValue function tested
    SetPlaneValue function is tested with media_test.csv
    The code is:
    [f_nodes,Logic_M] = ReadMedia('media_test.csv',4,5,4);
    [f,eq,vel,rho,cx,cy,cz] = VarSetUp(f_nodes,15,3);
    [vel{1}] = SetPlaneValue('xy1',9,vel{1},4,5,4,Logic_M);

--- Code/SetPlaneValue.m
+++ Code/SetPlaneValue.m
@@ -19,37 +19,49 @@ switch plane
     case 'xy1'
         temp_u = zeros(m,n,o);
         temp_u(Logic_M) = u;
-        temp_u(Logic_M(:,:,1)) = u0;
+        f_plane_nodes = false(m,n,o);
+        f_plane_nodes(:,:,1) = Logic_M(:,:,1);
+        temp_u(f_plane_nodes) = u0;
         u = temp_u(Logic_M);
    
     case 'xy2'
         temp_u = zeros(m,n,o);
         temp_u(Logic_M) = u;
-        temp_u(Logic_M(:,:,o)) = u0;
+        f_plane_nodes = false(m,n,o);
+        f_plane_nodes(:,:,o) = Logic_M(:,:,o);
+        temp_u(f_plane_nodes) = u0;
         u = temp_u(Logic_M);
         
     case 'xz1'
         temp_u = zeros(m,n,o);
         temp_u(Logic_M) = u;
-        temp_u(Logic_M(:,1,:)) = u0;
+        f_plane_nodes = false(m,n,o);
+        f_plane_nodes(:,1,:) = Logic_M(:,1,:);
+        temp_u(f_plane_nodes) = u0;
         u = temp_u(Logic_M);
         
     case 'xz2'
         temp_u = zeros(m,n,o);
         temp_u(Logic_M) = u;
-        temp_u(Logic_M(:,n,:)) = u0;
+        f_plane_nodes = false(m,n,o);
+        f_plane_nodes(:,n,:) = Logic_M(:,n,:);
+        temp_u(f_plane_nodes) = u0;
         u = temp_u(Logic_M);
         
     case 'yz1'
         temp_u = zeros(m,n,o);
         temp_u(Logic_M) = u;
-        temp_u(Logic_M(1,:,:)) = u0;
+        f_plane_nodes = false(m,n,o);
+        f_plane_nodes(1,:,:) = Logic_M(1,:,:);
+        temp_u(f_plane_nodes) = u0;
         u = temp_u(Logic_M);
         
     case 'yz2'
         temp_u = zeros(m,n,o);
         temp_u(Logic_M) = u;
-        temp_u(Logic_M(m,:,:)) = u0;
+        f_plane_nodes = false(m,n,o);
+        f_plane_nodes(m,:,:) = Logic_M(m,:,:);
+        temp_u(f_plane_nodes) = u0;
         u = temp_u(Logic_M);
 end
  
