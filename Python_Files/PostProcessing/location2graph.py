e129e4d162dd5780ff96ee25b0ca0911a261c14e
jcmatailo <jmatailov@gmail.com>
2021-06-07 00:55:55 -0500

    Add comments to the location2graph and move_files codes

--- Python_Files/PostProcessing/location2graph.py
+++ Python_Files/PostProcessing/location2graph.py
@@ -2,11 +2,13 @@
 ''' Created on Sun Mar 14 00:09:39 2021
 @author: Ayrton M and jmatailov11 '''
 
+'''
+This function returns the indices where the '1' values are found.
+'''
+
 import numpy as np
 
-# This function returns the indices where the 1 values are found:
-
 def location(loc_matrix):
-    location = np.where(loc_matrix == 1)                                                                                                                                                                                                                                                
-    location = np.transpose(np.array(location))     
+    location = np.where(loc_matrix == 1)           # return indices of the values that are '1'                                                                                                                                                                                                                       
+    location = np.transpose(np.array(location))    # reverse the axes of an matrix
     return location
