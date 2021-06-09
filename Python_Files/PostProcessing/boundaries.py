f9a45d89942c87e9d3e2d82803b13c4ca3dbac44
jcmatailo <jmatailov@gmail.com>
2021-06-09 00:13:51 -0500

    Correct mistakes and improve comments of PostProcessing folder.

--- Python_Files/PostProcessing/boundaries.py
+++ Python_Files/PostProcessing/boundaries.py
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
 
 ''' Created on Sun Mar 14 00:09:39 2021
-@author: Ayrton M and jmatailov11 '''
+@author: Ayrton M and jmatailov11
 
-'''
 This code take a media matrix (3D array) as input,
 and return the boundaries of that media as matrix
 using binary dilation.
 
 Binary dilation method return "True" and "False" values,
 so it is necessary to convert these values to "0" and "1",
-this is achieved by multiplying  by 1.
-'''
+this is achieved by multiplying  by 1. '''
 
-import numpy as np  
+import numpy as np
 from scipy.ndimage.morphology import binary_dilation
 
-def boundaries(media):
-    k = np.ones(shape=(3,3,3))                          	# Create a ones matrix (3x3x3)
+def boundaries(media):                                      
+    k = np.ones(shape=(3,3,3))                          	    # Create a ones matrix (3x3x3)
     boundaries_media = binary_dilation(media, structure=k)    	# Binary dilation is applied to the input matrix
-    boundaries_media = boundaries_media * 1			# "True" values to "1"
+    boundaries_media = boundaries_media * 1			            # "True" values to "1"
     boundaries_media = boundaries_media - media             	# Boundaries matrix generated
     return boundaries_media
--- Python_Files/PostProcessing/move_files.py
+++ Python_Files/PostProcessing/move_files.py
@@ -1,28 +1,26 @@
 # -*- coding: utf-8 -*-
-""" Created on Sat May 29 23:49:41 2021
-@author: Ayrton M and jcmatailo"""
+''' Created on Sat May 29 23:49:41 2021
+@author: Ayrton M and jcmatailo
 
-'''
-Move .csv files to a new directory to sort the documents. 
-'''
+Move .csv files to a new directory to sort the documents. '''
 
 import shutil
 import os
 
 def move_file(file, folder):
 
-    file = file + '.csv'                  # add ".csv" to the file name
-    current_path = os.getcwd() + os.sep   # add the current working diretory and path name separator ("/")
+    file = file + '.csv'                         # add ".csv" to the file name
+    current_path = os.getcwd() + os.sep          # add the current working diretory and path name separator ("/")
 
-    status_folder = os.path.exists(folder)   # check whether the path exists or not
-    if (status_folder != True):              
-        os.makedirs(current_path+folder)     # make the directory specified if the file does not exist
+    status_folder = os.path.exists(folder)       # check whether the path exists or not
+    if (status_folder != True):
+        os.makedirs(current_path+folder)         # make the directory specified if the file does not exist
 
-    source = current_path + file             # from this path will be copied the file
-    destination = current_path + folder + os.sep + file  # destination path
-    
-    old_file = os.path.exists(destination)   # destination path is assigned to other variable 
+    source = current_path + file                 # from this path will be copied the file
+    destination = current_path + folder + os.sep + file     # destination path
+
+    old_file = os.path.exists(destination)       # destination path is assigned to other variable
     if (old_file == True):
-        os.remove(destination)   # verify that does not exists similar files
-   
-    shutil.move(source, destination)   # move the file from source path to destination path
+        os.remove(destination)                   # verify that does not exists similar files
+
+    shutil.move(source, destination)             # move the file from source path to destination path
