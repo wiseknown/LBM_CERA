7b8985d8b1213d1275f05f1b01c491f1e01c4b1b
jcmatailo <jmatailov@gmail.com>
2021-06-19 17:29:53 -0500

    Edit boundaries.py

--- Python_Files/PostProcessing/boundaries.py
+++ Python_Files/PostProcessing/boundaries.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ''' Created on Sun Mar 14 00:09:39 2021
-@author: Ayrton M and jmatailov11
+@author: Ayrton M and jcmatailo
 
 This code take a media matrix (3D array) as input,
 and return the boundaries of that media as matrix
@@ -14,7 +14,7 @@ this is achieved by multiplying  by 1. '''
 import numpy as np
 from scipy.ndimage.morphology import binary_dilation
 
-def boundaries(media):                                      
+def boundaries(media):
     k = np.ones(shape=(3,3,3))                          	    # Create a ones matrix (3x3x3)
     boundaries_media = binary_dilation(media, structure=k)    	# Binary dilation is applied to the input matrix
     boundaries_media = boundaries_media * 1			            # "True" values to "1"
