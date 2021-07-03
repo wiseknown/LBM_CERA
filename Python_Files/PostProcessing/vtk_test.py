11744974d09f2f30f43cd34f14c3f9713da054ec
Jean Carlos Matailo <77908421+jcmatailo@users.noreply.github.com>
2021-07-03 14:26:19 -0500

    Update vtk_test.py file

--- Python_Files/PostProcessing/vtk_test.py
+++ Python_Files/PostProcessing/vtk_test.py
@@ -1,26 +1,26 @@
-import numpy as np
-import vtk
+# Libraries
+import numpy as np                      # Import numpy library
+import vtk                              # Import vtk library
 from vtk.util import numpy_support
 
+# Start function for render
 def render_marchingCubes(array3D):
 
-    arr2rnd = array3D
+    arr2rnd = array3D                   # New variable that store the input
 
-    arrayMax = np.max(arr2rnd)
-    arrayMin = np.min(arr2rnd)
-    arr_range = arrayMax - arrayMin
+    arrayMax = np.max(arr2rnd)          # Select the maximum value of the input array
+    arrayMin = np.min(arr2rnd)          # Select the minimun value of the input array
+    arr_range = arrayMax - arrayMin     # Obtain the range of the input array
 
-    # numpy normalized
-    arr2rnd_norm = (arr2rnd - arrayMin) / arr_range
+    arr2rnd_norm = (arr2rnd - arrayMin) / arr_range     # Numpy normalized
 
-    arr2rnd_shape = arr2rnd_norm.shape
+    arr2rnd_shape = arr2rnd_norm.shape      # array shape
+    # Converting a numpy array to vtk array
     arr2rnd_vtk = numpy_support.numpy_to_vtk(num_array=arr2rnd_norm.ravel(), deep=True, array_type=vtk.VTK_FLOAT)
-    """
-    """
 
-    vtk_data = vtk.vtkImageData()
-    vtk_data.SetOrigin(0, 0, 0)             # set origin as necessary
-    vtk_data.SetSpacing(1, 1, 1)            # set spacing as necessary
+    vtk_data = vtk.vtkImageData()           
+    vtk_data.SetOrigin(0, 0, 0)             # Set origin as necessary
+    vtk_data.SetSpacing(1, 1, 1)            # Set spacing as necessary
     vtk_data.SetDimensions(arr2rnd_shape[2], arr2rnd_shape[1], arr2rnd_shape[0])
     vtk_data.AllocateScalars(numpy_support.get_vtk_array_type(arr2rnd_vtk.dtype),1)
     vtk_data.GetPointData().SetScalars(arr2rnd_vtk)
